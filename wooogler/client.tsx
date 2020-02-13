b2f3dc8aef4b76da4dd9017293f4f9eda363cd49
wooogler <leesang627@gmail.com>
2020-02-13 17:43:55 +0900

    Lotto finished!

--- wooogler/client.tsx
+++ wooogler/client.tsx
@@ -6,8 +6,9 @@ import WordRelay from './WordRelay';
 import NumberBaseball from './NumberBaseball';
 import ResponseCheck from './ResponseCheckClass';
 import RSP from './RSP';
+import Lotto from './LottoClass';
 import { hot } from 'react-hot-loader/root';
 
-const Hot = hot(RSP);
+const Hot = hot(Lotto);
 
 ReactDOM.render(<Hot />, document.querySelector('#root'));

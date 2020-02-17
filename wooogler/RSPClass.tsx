4149c07f482a6dc46be80afd6aff25e9cd9e458e
wooogler <leesang627@gmail.com>
2020-02-17 16:25:27 +0900

    Update RSPClass.tsx

--- wooogler/RSPClass.tsx
+++ wooogler/RSPClass.tsx
@@ -83,13 +83,14 @@ class RSP extends Component<{}, IState> {
     }, 1000);
   };
   render() {
+    const {imgCoord, result, score} = this.state;
     return (
       <>
         <div id="computer" style={{ background: `url(https://en.pimg.jp/023/182/267/1/23182267.jpg) ${imgCoord} 0` }} />
         <div>
-          <button id="rock" className="btn" onClick={onClickBtn('바위')}>바위</button>
-          <button id="scissor" className="btn" onClick={onClickBtn('가위')}>가위</button>
-          <button id="paper" className="btn" onClick={onClickBtn('보')}>보</button>
+          <button id="rock" className="btn" onClick={this.onClickBtn('바위')}>바위</button>
+          <button id="scissor" className="btn" onClick={this.onClickBtn('가위')}>가위</button>
+          <button id="paper" className="btn" onClick={this.onClickBtn('보')}>보</button>
         </div>
         <div>{result}</div>
         <div>현재 {score}점</div>
